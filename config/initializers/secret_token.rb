eaf750d44241db774f311160e757b4d70ab83ef0
Joseph Pirtle <pirtlj@gmail.com>
2013-02-06 20:37:27 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metacraft::Application.config.secret_token = '78597f1b0b3e8d4e0c44ae77304d67e9f622d7ac5331344cfd17e9f7cc5f4df5e919d30bae5e33d2517e0c72b1d2c092838d17e3157a55c0234a1ec67a6843d5'
